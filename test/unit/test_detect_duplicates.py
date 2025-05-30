adfdfb705eb43baafa891105a787153d9b2dbaf6
Isak Klaesson <isak.klaesson2002@gmail.com>
2025-05-30 16:27:28 +0200

    Test 1

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -6,14 +6,15 @@ from src.util.detector import detect_duplicates
 @pytest.mark.unit
 def test_detect_duplicates():
     data ={
-        "key":"frattini2023requirements",
-        "title":"Requirements quality research: a harmonized theory, evaluation, and roadmap",
-        "author":"Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael",
-        "Journal":"Requirements Engineering",
-        "pages":"1--14",
-        "year":"2023",
-        "publisher":"Springer",
-        "doi":"10.1007/s00766-023-00405-y"
+        """@article{frattini2023requirements,
+	title={Requirements quality research: a harmonized theory, evaluation, and roadmap},
+	  author={Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael},
+	  journal={Requirements Engineering},
+	  pages={1--14},
+	  year={2023},
+	  publisher={Springer},
+	  doi={10.1007/s00766-023-00405-y}
+}"""
     }
 
     assert detect_duplicates(data) == ValueError
