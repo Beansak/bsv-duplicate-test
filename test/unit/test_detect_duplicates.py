bdc61616af1f7ba7f497496db26ad5bae959d68e
Isak Klaesson <isak.klaesson2002@gmail.com>
2025-05-30 16:25:27 +0200

    Test 1

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,7 +1,19 @@
 import pytest
-
+from unittest.mock import patch, MagicMock
+from src.util.detector import detect_duplicates
 # develop your test cases here
 
 @pytest.mark.unit
 def test_detect_duplicates():
-    assert True
+    data ={
+        "key":"frattini2023requirements",
+        "title":"Requirements quality research: a harmonized theory, evaluation, and roadmap",
+        "author":"Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael",
+        "Journal":"Requirements Engineering",
+        "pages":"1--14",
+        "year":"2023",
+        "publisher":"Springer",
+        "doi":"10.1007/s00766-023-00405-y"
+    }
+
+    assert detect_duplicates(data) == ValueError
