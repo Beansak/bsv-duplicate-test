68e031817f520fa0a43aece93a28f1f519cf18c9
Isak Klaesson <isak.klaesson2002@gmail.com>
2025-05-30 16:45:09 +0200

    test

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -20,11 +20,11 @@ def test_one_entry():
 
     with pytest.raises(ValueError):
         
-        duplicate = detect_duplicates(data)
+        detect_duplicates(data)
         
 
 @pytest.mark.unit
-def test_two_entries_same_doi():
+def test_duplicates_same_key():
     data = """
     @article{frattini2023requirements,
 	title={Requirements quality research: a harmonized theory, evaluation, and roadmap},
@@ -50,4 +50,29 @@ def test_two_entries_same_doi():
     duplicate = detect_duplicates(data)
     assert len(duplicate) == 1
 
-        
+
+@pytest.mark.unit    
+def test_duplicates_no_key():
+    data = """@article{wagner2019status,
+  title={Status quo in requirements engineering: A theory and a global family of surveys},
+  author={Wagner, Stefan and M{\'e}ndez Fern{\'a}ndez, Daniel and Felderer, Michael and Vetr{\`o}, Antonio and Kalinowski, Marcos and Wieringa, Roel and Pfahl, Dietmar and Conte, Tayana and Christiansson, Marie-Therese and Greer, Desmond and others},
+  journal={ACM Transactions on Software Engineering and Methodology (TOSEM)},
+  volume={28},
+  number={2},
+  pages={1--48},
+  year={2019},
+  publisher={ACM New York, NY, USA}
+}
+
+@article{frattini2023requirements,
+	title={Requirements quality research: a harmonized theory, evaluation, and roadmap},
+	  author={Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael},
+	  journal={Requirements Engineering},
+	  pages={1--14},
+	  year={2023},
+	  publisher={Springer}
+}"""
+
+    duplicate = detect_duplicates(data)
+    assert len(duplicate) == 1
+    
